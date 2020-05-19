68364a61acb1124ee0a0640bbd4a3a9faae02835
yeonggyulim <lyg940128@gmail.com>
2020-05-19 17:32:41 +0900

    add auth validation

--- blog/blog-frontend/src/components/auth/AuthForm.js
+++ blog/blog-frontend/src/components/auth/AuthForm.js
@@ -60,7 +60,17 @@ const textMap = {
     register: '회원가입',
 };
 
-const AuthForm = ({ type, form, onChange, onSubmit }) => {
+/**
+ * 에러를 보여 줍니다. 
+ */
+const ErrorMessage = styled.div`
+    color: red;
+    text-align: center;
+    font-size: 0.875rem;
+    margin-top: 1rem;
+`;
+
+const AuthForm = ({ type, form, onChange, onSubmit, error }) => {
     const text = textMap[type];
     return (
         <AuthFormBlock>
@@ -91,6 +101,7 @@ const AuthForm = ({ type, form, onChange, onSubmit }) => {
                         value={form.passwordConfirm}
                     />
                 )}
+                {error && <ErrorMessage>{error}</ErrorMessage>}
                 <ButtonWidthMarginTop cyan fullWidth>{text}</ButtonWidthMarginTop>
             </form>
             <Footer>
