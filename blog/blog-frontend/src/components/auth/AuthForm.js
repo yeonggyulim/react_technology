d7e43baed9255d4b3260b9617ee5418f1f9a9aa9
yeonggyulim <lyg940128@gmail.com>
2020-05-19 14:56:53 +0900

    Implement api and auth redux modules

--- blog/blog-frontend/src/components/auth/AuthForm.js
+++ blog/blog-frontend/src/components/auth/AuthForm.js
@@ -60,18 +60,26 @@ const textMap = {
     register: '회원가입',
 };
 
-const AuthForm = ({ type }) => {
+const AuthForm = ({ type, form, onChange, onSubmit }) => {
     const text = textMap[type];
     return (
         <AuthFormBlock>
             <h3>{text}</h3>
-            <form>
-                <StyledInput autoComplete='username' name='username' placeholder='아이디' />
+            <form onSubmit={onSubmit}>
+                <StyledInput
+                    autoComplete='username'
+                    name='username'
+                    placeholder='아이디'
+                    onChange={onChange}
+                    value={form.username}
+                />
                 <StyledInput
                     autoComplete='new-password'
                     name='password'
                     placeholder='비밀번호'
                     type='password'
+                    onChange={onChange}
+                    value={form.password}
                 />
                 {type === 'register' && (
                     <StyledInput
@@ -79,6 +87,8 @@ const AuthForm = ({ type }) => {
                         name='passwordConfirm'
                         placeholder='비밀번호 확인'
                         type='password'
+                        onChange={onChange}
+                        value={form.passwordConfirm}
                     />
                 )}
                 <ButtonWidthMarginTop cyan fullWidth>{text}</ButtonWidthMarginTop>
@@ -87,8 +97,8 @@ const AuthForm = ({ type }) => {
                 {type === 'login' ? (
                     <Link to='/register'>회원가입</Link>
                 ) : (
-                    <Link to='/login'>로그인</Link>
-                )}
+                        <Link to='/login'>로그인</Link>
+                    )}
             </Footer>
         </AuthFormBlock>
     );
---
+++ blog/blog-frontend/src/containers/auth/RegisterForm.js
@@ -0,0 +1,65 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { changeField, initializeForm, register } from '../../modules/auth';
+import AuthForm from '../../components/auth/AuthForm';
+
+const RegisterForm = () => {
+    const dispatch = useDispatch();
+    const { form, auth, authError } = useSelector(({ auth }) => ({
+        form: auth.register,
+        auth: auth.auth,
+        authError: auth.authError
+    }));
+
+    // 인풋 변경 이벤트 핸들러
+    const onChange = e => {
+        const { value, name } = e.target;
+        dispatch(
+            changeField({
+                form: 'register',
+                key: name,
+                value
+            })
+        );
+    };
+
+    // 폼 등록 이벤트 핸들러
+    const onSubmit = e => {
+        e.preventDefault();
+        const { username, password, passwordConfirm } = form;
+        if (password !== passwordConfirm) {
+            // TODO: 오류 처리
+            return;
+        }
+        dispatch(register({ username, password }));
+    };
+
+    // 컴포넌트가 처음 렌더링될 때 form을 초기화함
+    useEffect(() => {
+        dispatch(initializeForm('register'));
+    }, [dispatch]);
+
+    // 회원가입 성공/실패 처리
+    useEffect(() => {
+        if (authError) {
+            console.log('오류 발생');
+            console.log(authError);
+            return ;
+        }
+        if (auth) {
+            console.log('회원가입 성공');
+            console.log(auth);
+        }
+    }, [auth, authError]);
+
+    return (
+        <AuthForm
+            type='register'
+            form={form}
+            onChange={onChange}
+            onSubmit={onSubmit}
+        />
+    );
+};
+
+export default RegisterForm;
