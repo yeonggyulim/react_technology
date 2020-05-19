4b7f58dd4adc48da4efa2ff8f85a591437736f67
yeonggyulim <lyg940128@gmail.com>
2020-05-20 01:00:19 +0900

    add logout function and set local storage when to login

--- blog/blog-frontend/src/components/common/Header.js
+++ blog/blog-frontend/src/components/common/Header.js
@@ -5,10 +5,10 @@ import Button from './Button';
 import { Link } from 'react-router-dom';
 
 const HeaderBlock = styled.div`
-    position: fixed;
-    width: 100%;
-    background: white;
-    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.08);
+  position: fixed;
+  width: 100%;
+  background: white;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.08);
 `;
 
 /**
@@ -16,19 +16,19 @@ const HeaderBlock = styled.div`
  */
 
 const Wrapper = styled(Responsive)`
-    height: 4rem;
+  height: 4rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between; /* 자식 엘리먼트 사이의 여백을 최대로 설정 */
+  .logo {
+    font-size: 1.125rem;
+    font-weight: 800;
+    letter-spacing: 2px;
+  }
+  .right {
     display: flex;
     align-items: center;
-    justify-content: space-between; /* 자식 엘리먼트 사이의 여백을 최대로 설정 */
-    .logo {
-        font-size: 1.125rem;
-        font-weight: 800;
-        letter-spacing: 2px;
-    }
-    .right {
-        display: flex;
-        align-items: center;
-    }
+  }
 `;
 
 /**
@@ -36,25 +36,37 @@ const Wrapper = styled(Responsive)`
  */
 
 const Spacer = styled.div`
-    height: 4rem;
+  height: 4rem;
 `;
 
-const Header = () => {
-    return (
-        <>
-            <HeaderBlock>
-                <Wrapper>
-                    <Link to='/' className='logo'>
-                        REACTERS
-                    </Link>
-                    <div className='right'>
-                        <Button to='/login'>로그인</Button>
-                    </div>
-                </Wrapper>
-            </HeaderBlock>
-            <Spacer />
-        </>
-    );
+const UserInfo = styled.div`
+  font-weight: 800;
+  margin-right: 1rem;
+`;
+
+const Header = ({ user, onLogout }) => {
+  return (
+    <>
+      <HeaderBlock>
+        <Wrapper>
+          <Link to="/" className="logo">
+            REACTERS
+          </Link>
+          {user ? (
+            <div className="right">
+              <UserInfo>{user.username}</UserInfo>
+              <Button onClick={onLogout}>로그아웃</Button>
+            </div>
+          ) : (
+            <div className="right">
+              <Button to="/login">로그인</Button>
+            </div>
+          )}
+        </Wrapper>
+      </HeaderBlock>
+      <Spacer />
+    </>
+  );
 };
 
 export default Header;
--- blog/blog-frontend/src/pages/PostListPage.js
+++ blog/blog-frontend/src/pages/PostListPage.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Header from '../components/common/Header';
+import HeaderContainer from '../containers/common/HeaderContainer';
 
 const PostListPage = () => {
   return (
     <>
-      <Header />
+      <HeaderContainer />
       <div>안녕하세요.</div>
     </>
   );
