0482a50e6c30addf2c558f4b5c6da8db52fc7d1d
yeonggyulim <lyg940128@gmail.com>
2020-05-19 12:41:14 +0900

    Implement Auth Form UI(LoginPage and RegisterPage)

--- blog/blog-frontend/src/pages/LoginPage.js
+++ blog/blog-frontend/src/pages/LoginPage.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import AuthTemplate from '../components/auth/AuthTemplate';
+import AuthForm from '../components/auth/AuthForm';
 
 const LoginPage = () => {
-  return <div>로그인</div>;
+  return (
+    <AuthTemplate>
+      <AuthForm type='login' />
+    </AuthTemplate>
+  );
 };
 
 export default LoginPage;
